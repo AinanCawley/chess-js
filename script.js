f525a6cb2d6d5ee5ef4d035f424a8e48cebbad3e
AinanCawley <ainan.c.cawley@gmail.com>
2022-05-10 09:48:23 +0800

    Give each square its coordinate as id

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const numberToLetter = function(number)
+{ //for converting a number between "0" and "7" to a letter between "a" and "h"
+    if(number==0)
+    {
+        return "a";
+    }
+    if(number==1)
+    {
+        return "b";
+    }
+    if(number==2)
+    {
+        return "c";
+    }
+    if(number==3)
+    {
+        return "d";
+    }
+    if(number==4)
+    {
+        return "e";
+    }
+    if(number==5)
+    {
+        return "f";
+    }
+    if(number==6)
+    {
+        return "g";
+    }
+    if(number==7)
+    {
+        return "h";
+    }
+}
+
+//DOM STUFF
+
 const gameContainer = document.getElementById("gameContainer")
 const chessboard = document.createElement('div');
 const boardFragment = document.createDocumentFragment();
@@ -10,6 +48,12 @@ for( let i = 0; i < 8; i++ )
     {
         let square = document.createElement('div');
         square.classList.add("chessboardSquare");
+
+        square.setAttribute("id", numberToLetter(i) + "" + (8-j) );
+        //^^To give each square its standard coordinate name as id
+
+        square.innerText = square.id; //FOR CHECKING PURPOSES
+
         column.appendChild(square);
     }
 
