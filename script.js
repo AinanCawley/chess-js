b69f8b61e08ad73d47e62cdefe4bf66a040a5a5f
AinanCawley <ainan.c.cawley@gmail.com>
2022-05-15 23:10:10 +0800

    Change TODO comment for CheckChecker

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ const pseudolegalMovesFromConventionalBoard = function(conventionalBoardArray)
 }
 
 const isTheSideNotToMoveInCheckChecker = function(conventionalBoardArray, booleanToMove)
-{ // TODO: check for raytracing attacks TODO URGENT: Fix the QR raytracing to be blocked my opponent non-QR pieces TODO: King checks!
+{ // TODO: finish for Black to move
     if( booleanToMove==true )
     { // ^^This means White to move, so check if Black's King is in check
         let coordinatesArray = findCoordinatesOfKing(conventionalBoardArray,false);
