f9396cdd4784b8ab042f18e53b6d2622367a4f0f
AinanCawley <ainan.c.cawley@gmail.com>
2022-05-26 15:40:03 +0800

    Update TODO comment

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ const legalMovesFromConventionalBoard = function(conventionalBoardArray, boolean
 }
 
 const pseudolegalMovesFromConventionalBoard = function(conventionalBoardArray, booleanToMove, castlingRights, enpassantSquare)
-{ // TODO: ~~~ BLACK(everything) ~~~ WHITE(Pawn, King) ~~~
+{ // TODO: ~~~ BLACK(everything) ~~~ WHITE(Pawn) ~~~
     let arrayOfChecks = [];
     let arrayOfCaptures = [];
     let arrayOfOtherMoves = [];
